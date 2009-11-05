b9f489f0b6538b238feb95ca754fd6745f17ec17
hitode909 <hitode909@gmail.com>
2009-11-05 11:19:45 +0900

    trying job queue

--- sketch.rb
+++ sketch.rb
@@ -3,6 +3,18 @@ require 'pp'
 require 'pit'
 require 'module/nicovideo/lib/nicovideo'
 require 'erb'
+require 'thread'
+
+alias original_open open
+def open(*a)
+  while $last_open and Time.now - $last_open < 5
+    sec = $last_open + 5.0 - Time.now
+    p "waiting #{sec}sec"
+    sleep sec
+  end
+  $last_open = Time.now
+  original_open(*a)
+end
 
 module NicoPodcast
   def self.output_path
@@ -10,6 +22,13 @@ module NicoPodcast
   end
   def self.output_path=(a)
     @@output_path = a
+  end
+
+  def self.output_type
+    @@output_typeh ||= 'mp3'
+  end
+  def self.output_type=(a)
+    @@output_type = a
   end
 
   def self.file_directory
@@ -52,19 +71,23 @@ module NicoPodcast
     end
   end
 
-  class NicoPodcast <Podcast
+  class Podcast::Search <Podcast
     def process
       prepare_items
       publish
     end
 
     def prepare_items
-      @items.each{ |item|
+      @items = @items.map{ |item|
         begin
-          puts item.title
+          #puts item.title
           item.prepare
+          item
+        rescue => e
+          p e
+          nil
         end
-      }
+      }.compact
       return
     end
 
@@ -104,7 +127,7 @@ module NicoPodcast
 
     def prepare
       download unless has_source?
-      encode unless has_enclosure?
+      # encode unless has_enclosure?
     end
 
     def has_source?
@@ -116,7 +139,7 @@ module NicoPodcast
     end
 
     def download
-      puts "download #{@video.info.size_high / 1024}kb"
+      puts "download #{self.title} (#{@video.info.size_high / 1024}kb)"
       NicoPodcast.prepare_directory
       File.open(self.path, "wb") {|f|
         f.write @video.video
@@ -124,10 +147,10 @@ module NicoPodcast
     end
 
     def encode
-      puts 'encode mp3'
-      system "ffmpeg -i #{self.path} -acodec libmp3lame -ab 128k #{self.path('mp3')} > /dev/null" unless File.exist?(self.path('mp3'))
-      puts 'encode mp4'
-      system "ffmpeg -i #{self.path} -f mp4 -acodec libfaac -async 4800 -dts_delta_threshold 1 -vcodec libx264 -qscale 7 #{self.path('mp4')} > /dev/null" unless File.exist?(self.path('mp4'))
+      puts "encode #{self.title mp3}"
+      (system "ffmpeg -i #{self.path} -acodec libmp3lame -ab 128k #{self.path('mp3')} >& /dev/null" or File.unlink(self.path('mp3'))) unless File.exist?(self.path('mp3'))
+      puts "encode #{self.title mp4}"
+      (system "ffmpeg -i #{self.path} -f mp4 -acodec libfaac -async 4800 -dts_delta_threshold 1 -vcodec libx264 -qscale 7 #{self.path('mp4')} >& /dev/null" or File.unlink(self.path('mp4'))) unless File.exist?(self.path('mp4'))
     end
 
     def path(type = @video.type)
@@ -155,7 +178,8 @@ module NicoPodcast
       @video.info.description
     end
 
-    def enclosure_url(type = 'mp3')
+    def enclosure_url(type = NicoPodcast.output_type)
+      suffix = type ? ".#{type}" : ''
       File.join(NicoPodcast.root_url, NicoPodcast.file_directory, @video.video_id + suffix)
     end
 
@@ -192,8 +216,8 @@ end
 
 # ----------------------
 
-NicoPodcast.root_url = 'http://exampple.com/podcast/'
-podcast = NicoPodcast::NicoPodcast.new
+NicoPodcast.root_url = 'http://localhost/~fkd/podcast/'
+podcast = NicoPodcast::Podcast::Search.new
 key = 'capsule'
 search = NicoPodcast.agent.search(key)
 podcast.title = key
@@ -207,4 +231,6 @@ search.videos.map{ |vp|
   rescue
   end
 }
-puts podcast.process
+File.open("out.rss", "w") {|f|
+  f.puts podcast.process
+}
